f4d87f9cf96997b0d174916190179419a766eb16
Jordan Bean <jorbea@live.unc.edu>
2021-02-04 12:49:39 -0500

    ex01: Part 2 - Complete

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,17 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = 730151647
 
+x: int = int(input("Pick a number, any number..."))
 
-# Begin your solution here...
+if ((x % 2 == 0) or (x % 7 == 0)):
+    if (x % 2 == 0):
+        if (x % 7 == 0):
+            if ((x % 2 == 0) and (x % 7 == 0)):
+                print("TAR HEELS")
+        else:
+            print("TAR")
+    else:
+        print("HEELS")
+else:
+    print("CAROLINA")
