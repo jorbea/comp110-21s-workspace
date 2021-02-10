2bdbea3949f34bc46823c66ef2e45305a33a2afc
Jordan Bean <jorbea@live.unc.edu>
2021-02-10 00:12:46 -0500

    ex02_p1: tar_heels - Revised.01

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -3,20 +3,21 @@
 __author__ = "730151647"
 
 
-from random import choice
+# from random import choice
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
-    choice: int = int(input("Enter an int: "))
-    print(tar_heels(choice))
+    # choice: int = int(input("Enter an int: "))
+    response: int = int(input("Enter an int: "))
+    print(tar_heels(response))
 
 
-def tar_heels(choice: int) -> str:
-    """Integer from user is evaluated and a string is returned."""
-    if ((choice % 2 == 0) or (choice % 7 == 0)):
-        if (choice % 2 == 0):
-            if (choice % 7 == 0):
+def tar_heels(t: int) -> str:
+    """Integer from user input is evaluated based on its divisibility and a string is returned to main()."""
+    if ((t % 2 == 0) or (t % 7 == 0)):
+        if (t % 2 == 0):
+            if (t % 7 == 0):
                 return("TAR HEELS")
             else:
                 return("TAR")
