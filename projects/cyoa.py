0f0c733c44a31ac72357482ecf6d6c5653067050
Jordan Bean <jorbea@live.unc.edu>
2021-02-20 23:19:19 -0500

    PJ00: "The journey has begun!"

---
+++ projects/cyoa.py
@@ -0,0 +1,21 @@
+"""Choose your own adventure!"""
+
+__author__ = "730151647"
+
+def main() -> None:
+    greet()
+    points: int = x
+
+
+def greet(user: str) -> None:
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
